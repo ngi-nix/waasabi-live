2ef1b7ff1f3c3c280326a53c6994921b03b11663
ostylk <ostylk@googlemail.com>
2021-09-08 12:22:14 +0200

    compile correct version of esbuild binary for waasabi-live

--- flake.nix
+++ flake.nix
@@ -11,8 +11,21 @@
           pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
         in
         {
-
+          packages = {
+            inherit (pkgs) esbuild;
+          };
         }) // {
+      overlay = final: prev: {
+        esbuild = prev.esbuild.overrideAttrs (oldAttrs: rec {
+          version = "0.12.9";
 
+          src = final.fetchFromGitHub {
+            owner = "evanw";
+            repo = "esbuild";
+            rev = "v${version}";
+            sha256 = "sha256-MqwgdhgWIfYE0wO7fWQuC72tEwCVgL7qUbJlJ3APf4E=";
+          };
+        });
+      };
     };
 }
